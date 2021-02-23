d7570bf528395b1b528fee44bc8c9156a93b5c99
EvanSnyder2023 <816547@mystma.org>
2021-02-23 16:33:57 +0000

    revove evens try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(k):
+    myList = k
+    word = 0
+    for i in range(0, len(k)):
+        if myList[i - word] % 2 == 0 and myList[i - word] != 0:
+            myList.pop(i - word)
+            word = word + 1
+
+    return myList
+
+print (removeEvens([1,2,3,4]))
